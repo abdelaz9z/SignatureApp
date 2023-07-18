7eb163f6d0db20b922194eff415636c0230a8b84
Emad Omarah <emad.abdelhalem@hyperone.com.eg>
2023-07-18 18:02:05 +0300

    kk

--- app/src/main/java/com/casecode/signature/MainActivity.kt
+++ app/src/main/java/com/casecode/signature/MainActivity.kt
@@ -35,23 +35,38 @@ import java.io.FileOutputStream
 class MainActivity : AppCompatActivity() {
 
     private val PERMISSION_REQUEST_CODE = 100
-    val imageUrlMap = mutableMapOf<String, String>()
+    val signatureMap = mutableMapOf<String, Bitmap?>()
+    lateinit var imageView :ImageView
+    lateinit var imageView2 :ImageView
+
+    lateinit var createPdfButton1 :Button
+    lateinit var createPdfButton2 :Button
+    lateinit var mSignaturePad:SignaturePad
+    lateinit var KeeperSignedButton : Button
+    lateinit var receiverSignedButton : Button
+    lateinit var clearButton : Button
+    lateinit var positions :List<Pair<Float,Float>>
+
+
+    val orderID = "1111"
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val mSignaturePad = findViewById<SignaturePad>(R.id.signature_pad)
-        val startSigningButton = findViewById<Button>(R.id.btn_start_signing)
-        val signedButton = findViewById<Button>(R.id.btn_signed)
-        val clearButton = findViewById<Button>(R.id.btn_clear)
-
-
-        imageUrlMap[Constants.STORE_KEY] = Constants.IMAGE_URL
-        imageUrlMap[Constants.WORKER_KEY] = Constants.IMAGE_URL
-        imageUrlMap[Constants.KEEPER_KEY] = Constants.IMAGE_URL
-
+        imageView = findViewById<ImageView>(R.id.imageView3)
+        imageView2 = findViewById<ImageView>(R.id.imageView2)
+        createPdfButton1 = findViewById<Button>(R.id.create_pdf_1)
+        createPdfButton2 = findViewById<Button>(R.id.create_pdf_2)
+        mSignaturePad = findViewById<SignaturePad>(R.id.signature_pad)
+        KeeperSignedButton = findViewById<Button>(R.id.btn_start_signing)
+        receiverSignedButton = findViewById<Button>(R.id.btn_signed)
+        clearButton = findViewById<Button>(R.id.btn_clear)
+        positions = listOf(
+            Pair(5f, 5f),   // Position for "KEEPER_KEY"
+            //Pair(250f, 5f),  // Position for "WORKER_KEY"
+            Pair(450f, 5f)   // Position for "STORE_KEY"
+        )
         mSignaturePad.setOnSignedListener(object : SignaturePad.OnSignedListener {
             override fun onStartSigning() {
                 //Event triggered when the pad is touched
@@ -70,22 +85,28 @@ class MainActivity : AppCompatActivity() {
             mSignaturePad.clear()
         }
 
-        signedButton.setOnClickListener {
+        receiverSignedButton.setOnClickListener {
             val signatureBitmap = mSignaturePad.signatureBitmap
             val transparentSignatureBitmap = mSignaturePad.transparentSignatureBitmap
             val signatureSvg = mSignaturePad.signatureSvg
 
-            //addImageInPdfNew(transparentSignatureBitmap)
+            if (!transparentSignatureBitmap.equals(null)) {
+                saveReceiverSignatureOnServer(orderID,transparentSignatureBitmap)
+                mSignaturePad.clear()
+            }
         }
-
-        // Check if the bitmap is not null before using it
-
-        startSigningButton.setOnClickListener {
-            checkWriteStoragePermission()
+        KeeperSignedButton.setOnClickListener {
+            val transparentSignatureBitmap = mSignaturePad.transparentSignatureBitmap
+            if (!transparentSignatureBitmap.equals(null)) {
+                saveStorekeeperSignatureToServer(orderID,transparentSignatureBitmap)
+                mSignaturePad.clear()
+            }
         }
-
-
-        getSignature("123")
+        createPdfButton2.setOnClickListener {
+            getSignatureFromServer("1111")
+        }
+        createPdfButton1.setOnClickListener {
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -97,23 +118,16 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted, proceed with your task
-                downloadImageAndProcess(imageUrlMap)
+                addImageInPdf(signatureMap , positions)
             } else {
-                downloadImageAndProcess(imageUrlMap)
+                addImageInPdf(signatureMap , positions)
                 // Permission denied, handle it accordingly (e.g., show an error message)
             }
         }
     }
 
-    private fun receivingSignature(orderNumber: String, bitmap: Bitmap) {
-        val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-        //convert bitmap to byteArray
-        val receivingSignature = outputStream.toByteArray()
-        //convert byteArray to string
-        val binaryString = Base64.encodeToString(receivingSignature, Base64.DEFAULT)
-        //post image on DB
-        RetrofitClient.hyperoneApiService().receivingSignature(orderNumber, binaryString)
+    private fun saveReceiverSignatureOnServer(orderNumber: String, bitmap: Bitmap) {
+        RetrofitClient.hyperoneApiService().receivingSignature(orderNumber, bitmapToString(bitmap))
             .enqueue(object : Callback<ResponseBody> {
                 override fun onResponse(
                     call: Call<ResponseBody>,
@@ -124,7 +138,7 @@ class MainActivity : AppCompatActivity() {
 
                         // Handle the response
 
-                        Log.i("TAG", "status: $responseBody")
+                        Log.i("TAG", "receiver signature saved ")
                     } else {
                         // Handle the error
                         Log.d("TAG", "onResponse() returned: ${response.message()}")
@@ -138,15 +152,8 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
-    private fun storekeeperSignature(orderNumber: String, bitmap: Bitmap) {
-        val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-        val storekeeperSignatureByteArray = outputStream.toByteArray()
-
-        val binaryString = Base64.encodeToString(storekeeperSignatureByteArray, Base64.DEFAULT)
-
-
-        RetrofitClient.hyperoneApiService().storekeeperSignature(orderNumber, binaryString)
+    private fun saveStorekeeperSignatureToServer(orderNumber: String, bitmap: Bitmap) {
+        RetrofitClient.hyperoneApiService().storekeeperSignature(orderNumber, bitmapToString(bitmap))
             .enqueue(object : Callback<ResponseBody> {
                 override fun onResponse(
                     call: Call<ResponseBody>,
@@ -157,7 +164,7 @@ class MainActivity : AppCompatActivity() {
 
                         // Handle the response
 
-                        Log.i("TAG", "status: $responseBody")
+                        Log.i("TAG", "store keeper signature saved ")
                     } else {
                         // Handle the error
                         Log.d("TAG", "onResponse() returned: ${response.message()}")
@@ -169,100 +176,45 @@ class MainActivity : AppCompatActivity() {
                     Log.d("TAG", "Handle the error: ${t.message}")
                 }
             })
-    }
-
-    private fun downloadImageAndProcess(imageUrl: Map<String, String>) {
-        val bitmaps = mutableMapOf<String, Bitmap>()
-
-        val target = object : com.squareup.picasso.Target {
-            @RequiresApi(Build.VERSION_CODES.O)
-            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
-                if (bitmap != null) {
-                    val resizedBitmap = Bitmap.createScaledBitmap(bitmap, 50, 50, false)
-                    for ((key, value) in imageUrl) {
-                        if (value == imageUrl[key]) {
-                            bitmaps[key] = resizedBitmap
-                        }
-                    }
-
-                    Log.i("TAG", "onBitmapLoaded: $resizedBitmap")
-
-                    if (bitmaps.size == imageUrl.size) {
-                        val positions = listOf(
-                            Pair(20f, 5f),   // Position for "KEEPER_KEY"
-                            Pair(250f, 5f),  // Position for "WORKER_KEY"
-                            Pair(470f, 5f)   // Position for "STORE_KEY"
-                        )
-
-
-                        receivingSignature("123", resizedBitmap)
-//                        storekeeperSignature("123", resizedBitmap)
-
-                        addImageInPdf(bitmaps, positions)
-                    }
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Failed to download image",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-
-            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
-                Toast.makeText(this@MainActivity, "Failed to download image", Toast.LENGTH_SHORT)
-                    .show()
-            }
-
-            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
-                // Do nothing
-            }
-        }
-
-        for ((_, imageUrlValue) in imageUrl) {
-            Picasso.get().load(imageUrlValue).into(target)
-        }
     }
 
     private fun checkWriteStoragePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, proceed with your task
-                downloadImageAndProcess(imageUrlMap)
+                addImageInPdf(signatureMap , positions)
             } else {
                 requestPermissions(
                     arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
                     PERMISSION_REQUEST_CODE
                 )
+                addImageInPdf(signatureMap , positions)
             }
         } else {
             // For devices running below Android M, no runtime permission is required
-            downloadImageAndProcess(imageUrlMap)
+            addImageInPdf(signatureMap , positions)
         }
     }
 
-    private fun addImageInPdf(bitmaps: Map<String, Bitmap>, positions: List<Pair<Float, Float>>) {
+    private fun addImageInPdf(bitmaps: MutableMap<String, Bitmap?>, positions: List<Pair<Float, Float>>) {
         // Add image to PDF file
-        val downloadsDir =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        val downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         val outputFile = File(downloadsDir, "HyperOne.pdf")
-        val outputStream =
-            FileOutputStream(
-                File(
-                    downloadsDir,
-                    "modified.pdf"
-                )
-            ) // Specify the output file name or path
+        val outputStream = FileOutputStream(File(downloadsDir, "modified.pdf")) // Specify the output file name or path
         val reader = PdfReader(outputFile.inputStream())
         val stamper = PdfStamper(reader, outputStream)
 
         for (index in 1..reader.numberOfPages) {
             for ((key, bitmap) in bitmaps) {
                 val stream = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                bitmap?.compress(Bitmap.CompressFormat.PNG, 100, stream)
                 val image = Image.getInstance(stream.toByteArray())
-                for (pos in 0 until positions.size) {
-                    val (x, y) = positions[pos]
+                if (key.equals( Constants.WORKER_KEY)){
+                    val (x, y) = positions[0]
+                    image.setAbsolutePosition(x, y)
+                    val content = stamper.getOverContent(index)
+                    content.addImage(image)
+                }else{
+                    val (x, y) = positions[1]
                     image.setAbsolutePosition(x, y)
                     val content = stamper.getOverContent(index)
                     content.addImage(image)
@@ -275,7 +227,7 @@ class MainActivity : AppCompatActivity() {
         outputStream.close()
     }
 
-    private fun getSignature(orderNumber: String = "123") {
+    private fun getSignatureFromServer(orderNumber: String) {
         RetrofitClient.hyperoneApiService().getSignature(orderNumber)
             .enqueue(object : Callback<SignatureResponse> {
                 override fun onResponse(
@@ -289,18 +241,15 @@ class MainActivity : AppCompatActivity() {
                             // Handle the response
                             val receivingSignature = signatureList[0].receivingSignature
                             val storekeeperSignature = signatureList[0].storekeeperSignature
+                            imageView.setImageBitmap(stringToBitmap(receivingSignature))
+                            imageView2.setImageBitmap(stringToBitmap(storekeeperSignature))
+                            val resizedReceivingSignature = Bitmap.createScaledBitmap(stringToBitmap(receivingSignature), 80, 80, false)
+                            val resizedStorekeeperSignature = Bitmap.createScaledBitmap(stringToBitmap(storekeeperSignature), 80, 80, false)
 
+                            signatureMap[Constants.KEEPER_KEY] = resizedReceivingSignature
+                            signatureMap[Constants.WORKER_KEY] = resizedStorekeeperSignature
+                            checkWriteStoragePermission()
 
-                            Log.i("TAG", "onResponse: $receivingSignature")
-
-                            val byteArray = Base64.decode(receivingSignature, Base64.DEFAULT)
-                            Log.i("TAG", "byteArray: ${byteArray.size.toString()}")
-
-                            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-
-                            Log.i("TAG", "bitmap: ${bitmap.toString()}")
-                            val imageView = findViewById<ImageView>(R.id.imageView2)
-                            imageView.setImageBitmap(bitmap)
                         }
                     } else {
                         // Handle the error
@@ -314,15 +263,16 @@ class MainActivity : AppCompatActivity() {
                 }
             })
     }
-
-    fun String.hexStringToByteArray(): ByteArray {
-        val len = length
-        val data = ByteArray(len / 2)
-        for (i in 0 until len step 2) {
-            val byte =
-                ((Character.digit(this[i], 16) shl 4) + Character.digit(this[i + 1], 16)).toByte()
-            data[i / 2] = byte
-        }
-        return data
+    private fun bitmapToString(bitmap: Bitmap): String {
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream) // You can choose a different compression format and quality if desired
+        val byteArray = outputStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
+
+    private fun stringToBitmap(imageString: String): Bitmap {
+        val imageData: ByteArray = Base64.decode(imageString, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(imageData, 0, imageData.size)
+    }
+
 }
